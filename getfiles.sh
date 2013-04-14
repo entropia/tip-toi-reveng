6def3bd09e49b70e4f2f972003f31b34e09811e3
Joachim Breitner <mail@joachim-breitner.de>
2013-04-14 21:56:45 +0200

    Add source URL

--- getfiles.sh
+++ getfiles.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# From http://www.ravensburger.de/start/problembehebung/downloads/buecher/index.html
+
 wget -c 'http://static.tiptoi.com/db/applications/WWW%20Feuerwehr.gme' -O 'WWW_Feuerwehr.gme'
 wget -c 'http://static.tiptoi.com/db/applications/WWW%20Bauernhof.gme' -O 'WWW_Bauernhof.gme'
 wget -c 'http://static.tiptoi.com/db/applications/Leserabe%20Feen.gme' -O 'Leserabe_een.gme'
+
+wget -c http://static.tiptoi.com/db/firmware/update.upd
+wget -c http://static.tiptoi.com/db/firmware/Update3202.upd
