fdffdc9b2e1a8ea541c70153b1f187a34373e596
Joachim Breitner <mail@joachim-breitner.de>
2013-04-14 20:07:42 +0200

    Add Bauernhof

--- getfiles.sh
+++ getfiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 wget -c 'http://static.tiptoi.com/db/applications/WWW%20Feuerwehr.gme' -O 'WWW_Feuerwehr.gme'
+wget -c 'http://static.tiptoi.com/db/applications/WWW%20Bauernhof.gme' -O 'WWW_Bauernhof.gme'
