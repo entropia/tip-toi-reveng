1867fd4de4c1c0a701dcd5215d0548a965b3e287
Joachim Breitner <mail@joachim-breitner.de>
2013-04-14 20:43:26 +0200

    Extract ogg file

--- decode.hs
+++ decode.hs
@@ -5,10 +5,11 @@ import Data.Binary.Get
 import Data.Word
 import Text.Printf
 import Data.Bits
+import Data.Char
 
 oggTableOffset :: Get Word32
 oggTableOffset = do
-    skip 512
+    skip 4
     getWord32le
 
 oggTable :: Word32 -> Get (Word32, Word32)
@@ -30,6 +31,9 @@ getXor off = do
     let wanted = 79 :: Word8
     return $ wanted `xor` present
 
+magic :: B.ByteString
+magic = B.pack $ map (fromIntegral . ord) "OggS"
+
 main = do
     args <- getArgs
     file <- case args of
@@ -48,6 +52,10 @@ main = do
     printf "Ogg table offset: %08X\n" oto
     printf "Ogg table offset entry: %08X %d\n" oo ol
     printf "XOR value: %02X\n" x
+    putStr "XORed wantd magic: " >> mapM (printf "%02X") (B.unpack (B.map (xor x) magic)) >> putStrLn ""
     printf "Ogg magic: %s\n" (show (B.take 4 ogg))
     printf "Ogg magic xored: %s\n" (show (B.map (xor x) (B.take 4 ogg)))
+    let filename = file ++ printf "_%08x" oo ++ ".ogg"
+    B.writeFile filename (B.map (xor x) ogg)
+
 
