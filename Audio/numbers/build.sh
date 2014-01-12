ec9051d6dc356386e1c09dc445498621ebd0f14b
Joachim Breitner <mail@joachim-breitner.de>
2014-01-12 13:07:35 +0000

    Merge audio-replacing code by SuSonicTH
    And replace generated files by their sources and a script to generate
    them.
    
    This merges these commits (and thus closes #22):
    
    commit 9e1311b699dc9e0c9da17a5eba67a934fad93991
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 21:44:18 2014 +0100
    
        Update Readme.md
    
    commit 3ce4e77783bddd61aa3ba8a464e4f0838f9ca333
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 21:42:26 2014 +0100
    
        removed hex numbers
    
        removed hex numbers because of potencial licence issues
    
    commit e5a6eaaa5d3fb253183e0d68837aa1438182d1b1
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 21:11:28 2014 +0100
    
        Added licence information for Audio Files
    
    commit a51dfccc7802480d46ee4085b5d796d3af5c2faf
    Merge: 6a4b1d3 9b5330e
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 21:00:01 2014 +0100
    
        Merge branch 'master' of https://github.com/SuSonicTH/tip-toi-reveng
    
    commit 9b5330efb3888cafdefb0f3a8ad81fa33aae2059
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 02:46:31 2014 +0100
    
        Update Readme.md
    
    commit ba3f76cd62bad145c062d2ca220529c393623ef8
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 02:14:50 2014 +0100
    
        Update Readme.md
    
    commit 286f768055e1b6fefae2f088dac9a28ed00882a9
    Author: SuSonicTH <SuSonicTH@gmail.com>
    Date:   Fri Jan 10 02:05:43 2014 +0100
    
        Audio Files and func. to replace

---
+++ Audio/numbers/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This script creates ogg audio files speaking the numbers from 0000 to 1999 in
+# english.
+# Requires flac (package flac) and oggenc (package vorbis-tools)
+
+if test -d numbers/source
+then
+  cd numbers 
+fi
+
+if ! test -d source
+then
+  echo "Please run me in the numbers/ directory"
+  exit 1
+fi
+
+function create () {
+	out="$1";
+	shift;
+	echo "Creating $out.ogg..."
+	flac -s -d --endian little --sign signed --force-raw-format -c "$@" |
+	oggenc -Q --downmix --resample 22050 -r - -o $out.ogg
+}
+
+test -d numbers || mkdir numbers
+
+for d4 in $(seq 0 9); do
+create 000$d4 source/blob.flac source/english-$d4.flac
+done
+
+for d3 in $(seq 1 9); do
+for d4 in $(seq 0 9); do
+create 00$d3$d4 source/blob.flac source/english-{$d3,$d4}.flac
+done; done
+
+for d2 in $(seq 1 9); do
+for d3 in $(seq 0 9); do
+for d4 in $(seq 0 9); do
+create 0$d2$d3$d4 source/blob.flac source/english-{$d2,$d3,$d4}.flac
+done; done; done
+
+for d2 in $(seq 0 9); do
+for d3 in $(seq 0 9); do
+for d4 in $(seq 0 9); do
+create 1$d2$d3$d4 source/blob.flac source/english-{1,$d2,$d3,$d4}.flac
+done; done; done
---
+++ Audio/numbers/source/LICENSE.md
@@ -0,0 +1,12 @@
+english-*.flac:
+
+http://soundbible.com/2008-0-9-Male-Vocalized.html
+Recorded by Mike Koenig
+License: Attribution 3.0
+(Split and converted to FLAC by Joachim Breitner).
+
+blob.flac:
+http://soundbible.com/2067-Blop.html
+Recorded by Mark DiAngelo
+Attribution 3.0
+(Converted to FLAC by Joachim Breitner).
