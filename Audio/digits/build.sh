4a3cc314d6f377166c99b07944ec4fe03968d54b
Ulrich Sibiller <uli42@gmx.de>
2014-03-12 22:07:21 +0100

    code cleanup

--- Audio/digits/build.sh
+++ Audio/digits/build.sh
@@ -3,14 +3,10 @@
 # This script creates ogg audio files speaking the digits 0 to 9 in english.
 # Requires flac (package flac) and oggenc (package vorbis-tools)
 
-if test -d digits
-then
-  cd digits
-fi
+test -d digits && cd digits
 
-if ! test -d ../source
-then
-  echo "Please run me in the digits/ directory"
+if ! test -d ../source; then
+  echo >&2 "Please run me in the digits/ directory"
   exit 1
 fi
 
@@ -23,7 +19,6 @@ function create () {
 }
 
 create blob ../source/blob.flac
-for d in $(seq 0 9)
-do
+for d in {0..9}; do
 	create english-$d ../source/english-$d.flac
 done
