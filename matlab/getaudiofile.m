c75b12abc5cd919d6e3605bd2d393342ebcc69b8
Joachim Breitner <mail@joachim-breitner.de>
2015-02-20 21:23:27 +0100

    Remove matlap scripts
    with blessing from @maehw expressed in #90.

--- matlab/getaudiofile.m
+++
@@ -1,6 +0,0 @@
-function audiofile = getaudiofile(fid, offset, length)
-    fprintf('  Getting audio file from Offset: %d / 0x%08X, Length: %d / 0x%04X\n', ...
-        offset, offset, length, length);
-    fseek(fid, offset, 'bof');
-    audiofile = fread(fid, length, 'uint8');
-end
