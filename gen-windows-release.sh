5a864ee7b03b74551cc6f1df15d158d5e80c07c7
Joachim Breitner <mail@joachim-breitner.de>
2015-04-23 14:12:07 +0200

    Ship sox in the Windows release zipfile
    Works towards fixing #96

--- gen-windows-release.sh
+++ gen-windows-release.sh
@@ -5,17 +5,27 @@ wine cabal install --distdir=dist-win --bindir=.
 (cd Audio/digits/; ./build.sh)
 
 rev=$(git describe --tags)
-zipfile=tttool-win32-$rev.zip 
+zipfile=tttool-win32-$rev.zip
 rm -f $zipfile
 
-OGGENC=oggenc2.87-1.3.4-generic.zip 
+# Oggenc
+OGGENC=oggenc2.87-1.3.4-generic.zip
 
 mkdir -p contrib
 test -e contrib/$OGGENC ||
-    wget http://www.rarewares.org/files/ogg/$OGGENC -O contrib/$OGGENC 
+    wget http://www.rarewares.org/files/ogg/$OGGENC -O contrib/$OGGENC
 unzip -d contrib contrib/$OGGENC oggenc2.exe
-mv contrib/oggenc2.exe contrib/oggenc.exe 
+mv contrib/oggenc2.exe contrib/oggenc.exe
 
+# sox
+SOXVERSION=14.4.2
+SOXFILE=sox-$SOXVERSION-win32.zip
+
+test -e contrib/$SOXFILE ||
+	wget http://sourceforge.net/projects/sox/files/sox/$SOXVERSION/$SOXFILE/download -O contrib/$SOXFILE
+rm -rf contrib/sox contrib/sox-$SOXVERSION
+unzip -d contrib contrib/$SOXFILE
+mv contrib/sox-$SOXVERSION contrib/sox
 
 # install espeak first in wine
 cp ~/.wine/drive_c/Programme/eSpeak/command_line/espeak.exe contrib/
@@ -37,5 +47,6 @@ zip --recurse-paths $zipfile \
 	Audio/digits/*.ogg \
         contrib/oggenc.exe \
         contrib/espeak.exe \
-        contrib/espeak-data
+        contrib/espeak-data \
+	contrib/sox/*
 echo Created $zipfile
