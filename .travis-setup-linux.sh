6e46705af13e7925236cdccd0c7b82743a5fed4c
Joachim Breitner <mail@joachim-breitner.de>
2019-01-20 11:20:17 +0100

    Experiment with cabal new-style builds and caching

--- .travis-setup-linux.sh
+++ .travis-setup-linux.sh
@@ -3,9 +3,9 @@
 set -xe
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install cabal-install-$CABALVER ghc-$GHCVER # see note about happy/alex
+sudo apt-get install cabal-install-2.42.4VER # see note about happy/alex
 sudo apt-get install libxss-dev
 cabal --version
 echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
-cabal update
-cabal install --only-dependencies --enable-tests --enable-benchmarks
+cabal new-update
+cabal new-build --only-dependencies --enable-tests --enable-benchmarks
