eca703392c1a6021cab7ed84adaa382a9b380f6e
Joachim Breitner <mail@joachim-breitner.de>
2019-01-20 11:39:26 +0100

    typo

--- .travis-setup-linux.sh
+++ .travis-setup-linux.sh
@@ -3,7 +3,7 @@
 set -xe
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
-sudo apt-get install cabal-install-2.42.4 ghc-$GHCVER # see note about happy/alex
+sudo apt-get install cabal-install-2.4 ghc-$GHCVER # see note about happy/alex
 sudo apt-get install libxss-dev
 cabal --version
 echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
