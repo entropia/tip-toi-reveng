52d81e5581948579e17cb37ea383c314cdc82b3c
Joachim Breitner <mail@joachim-breitner.de>
2019-03-08 19:35:32 +0100

    Simplify(?) nix setup

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,14 @@
 let
   localLib = import ./lib.nix;
 
+  tttool-exe = args:
+    let
+      pkgs = localLib.iohkNix.getPkgs args;
+      haskell = localLib.nix-tools.haskell { inherit pkgs; };
+      nix-tools = import ./pkgs.nix { inherit haskell pkgs; };
+    in
+    nix-tools.tttool.components.exes.tttool;
 in
- { linux-exe = (localLib.nix-tools.default-nix ./pkgs.nix {}).nix-tools.exes.tttool;
-   windows-exe = (localLib.nix-tools.default-nix ./pkgs.nix { crossSystem = localLib.systems.examples.mingwW64; }).nix-tools.exes.tttool;
+ { linux-exe = tttool-exe {};
+   windows-exe = tttool-exe { crossSystem = localLib.systems.examples.mingwW64;};
  }
