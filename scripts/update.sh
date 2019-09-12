e5f5c7c5bd38c729625e3c5d4b79d6693431284b
Nicola Chiapolini <nicola.chiapolini@physik.uzh.ch>
2019-09-12 21:22:04 +0200

    update url of xml file
    It seems the xml-file has moved.

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,7 +6,7 @@ set -o nounset
 LANGUAGES="de_de fr_fr nl_nl it_it ru_ru"
 
 for lang in $LANGUAGES ; do 
-   wget -m -nd http://static.tiptoi.com/db/tiptoi_$lang.xml
+   wget -m -nd https://ssl-static.ravensburger.de/db/tiptoi_$lang.xml
    cat tiptoi_$lang.xml | `dirname $0`/download.pl | bash
    #cat tiptoi_$lang.xml | ./download.pl
 done
