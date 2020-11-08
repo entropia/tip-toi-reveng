135d1a69ee440c182ff431cd7a40255fa32a7c99
Joachim Breitner <mail@joachim-breitner.de>
2020-11-08 10:07:04 +0100

    Try nixpkgs 2009

--- nix/new.nix
+++ nix/new.nix
@@ -4,7 +4,7 @@
 # haskell.nix provides access to the nixpkgs pins which are used by our CI,
 # hence you will be more likely to get cache hits when using these.
 # But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 
 # haskell.nix provides some arguments to be passed to nixpkgs, including some
 # patches and also the haskell.nix functionality itself as an overlay.
