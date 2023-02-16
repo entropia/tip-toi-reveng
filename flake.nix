137981e852fa88e20535637619ac528c848b6ac3
Joachim Breitner <mail@joachim-breitner.de>
2023-02-16 11:09:46 +0100

    Dummy chnage

--- flake.nix
+++ flake.nix
@@ -30,9 +30,6 @@
   };
 
   nixConfig = {
-    # This sets the flake to use the IOG nix cache.
-    # Nix should ask for permission before using it,
-    # but remove it here if you do not want it to.
     extra-substituters = ["https://cache.iog.io"];
     extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
     allow-import-from-derivation = "true";
